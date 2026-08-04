5128c6cb899ac43fa20acaf77823497c110b7254
alvesgabryelle-crypto <alves.gabryelle@escola.pr.gov.br>
2026-08-04 10:06:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const botoes = document.querySelectorAll("button");
 
+botoes.forEach(function (botao) {
+    let curtiu = false;
+    botao.addEventListener("click", function () {
+        console.log("fui clicado");
+        let texto = botao.querySelector("span");
+        if (curtiu === false) {
+            texto.textContent++;
+            curtiu = true;
+            botao.style.backgroundColor = "#d9531e";
+            botao.style.color = "#ffffff";
+        } else {
+            texto.textContent--;
+            curtiu = false;
+            botao.style.backgroundColor = "transparent";
+            botao.style.color = "#000000";
+        }
+    });
+});
